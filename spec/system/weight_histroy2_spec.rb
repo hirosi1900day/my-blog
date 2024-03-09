ffb27a9843cecdb754cfcb5ae477e8d0342f7f79
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-09 13:01:50 +0900

    わざと失敗させていてたテストを削除

--- spec/system/weight_histroy2_spec.rb
+++ spec/system/weight_histroy2_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe WeightHistroy, type: :system do
-  context '失敗テスト確認' do
-    it '失敗' do
-      visit weight_histroys_path
+# RSpec.describe WeightHistroy, type: :system do
+#   context '失敗テスト確認' do
+#     it '失敗' do
+#       visit weight_histroys_path
 
-      expect(page).to_not have_content 'Weight histroy'
-    end
-  end
-end
+#       expect(page).to_not have_content 'Weight histroy'
+#     end
+#   end
+# end
